67e624a811a67eb7e4b2dc7129d482e0aed021fb
smbaresi <sumardiyanto.baresi@gmail.com>
2021-11-01 22:26:56 +0700

    fix typo and etc

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -12,18 +12,9 @@ import {
     Carousel
 } from 'react-bootstrap';
 
-
-import Img from './Aset/2077.png';
-import Img1 from './Aset/Group 46.png';
-import Img2 from './Aset/Group 47.png';
-import Img3 from './Aset/Group 48.png';
-import Img4 from './Aset/Group 49.png';
-import Img5 from './Aset/image 43.png';
-
 function Landing() {
     return (
-        <div>
-            <body>
+            <Container>
                 <Carousel>
                     <Carousel.Item>
                         <img
@@ -47,28 +38,20 @@ function Landing() {
                         />
                     </Carousel.Item>
                 </Carousel>
+                <br/>  <br/>
+                <img src="/Group-46.png"></img>
+                <img src="/Group-47.png"></img>
+                <img src="/Group-48.png"></img>
+                <img src="/Group-49.png"></img>
                 <h1 className="featured-text" style={{ fontSize: "medium", fontWeight: "bold" }}>
                     Featured Games
                 </h1>
-                <img src="/group-46"></img>
-                <img src="/group-47"></img>
-                <img src="/group-48"></img>
-                <img src="/group-49"></img>
+                <img src="/Group-46.png"></img>
+                <img src="/Group-47.png"></img>
+                <img src="/Group-48.png"></img>
+                <img src="/Group-49.png"></img>
                 <Button className="button-all-game" variant="outline-success" href="/">Show All Game</Button>
-                <Navbar fixed="bottom" expand="dark" variant="dark" bg="gray">
-                    <Container className="buttomnavbar">
-                        <Nav className="d-flex flex-row bd-highlight mb-3">
-                            <Navbar.Brand className="navbartext" href="/">Home</Navbar.Brand>
-                            <Navbar.Brand className="navbartext" href="">About</Navbar.Brand>
-                            <Navbar.Brand className="navbartext" href="">Contact Us</Navbar.Brand>
-                        </Nav>
-                        <Nav className="d-flex flex-row bd-highlight mb-3 d-flex align-items-end">
-                            <Navbar.Brand className="navbartext" href="">&copy; 2021</Navbar.Brand>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </body>
-        </div>
+            </Container>
     );
 }
 
