f1255d5a13e77d3017c42bcf3d1b256ed6e65fb1
KevinJhn <85426393+KevinJhn@users.noreply.github.com>
2021-11-02 00:05:33 +0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import routes from "../src/routes/index";
-import NavBar from "../src/Components/Navbar.js";
+// import NavBar from "../src/Components/Navbar.js";
 import Footer from "../src/Components/Footer.js";
 
 function App() {
   return (
     <Router>
-        <NavBar />
+        {/* <NavBar /> */}
         <Switch>
           {routes.map((route) => {
             return (
