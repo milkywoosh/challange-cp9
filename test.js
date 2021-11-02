9f4c19d58954ed69b64062e44a5f12c73e857b6e
milkywoosh <archer.lukman@gmail.com>
2021-11-02 21:58:02 +0700

    edit time

--- test.js
+++ test.js
@@ -1,68 +1,23 @@
+dataToDB = ['seri', 'kalah', 'menang']
 
-// input (gunting, kertas, batu);
+let getResult = (dataToDB) => {
+    counts = {};
 
-class Suitclass {
-
-    constructor() {
-        this.pool = ["gunting", "batu", "kertas"];
-        this.rand = Math.floor(Math.random() * this.pool.length);
-        this.inputP1 = null;
-        
-    }
-
-    getInputCOM() {
-        // return this.pool[this.rand];
-        console.log("computer: ", this.pool[this.rand])
-    }
-
-    getInputP1() {
-        // return this.inputP1;
-        console.log("player 1: ", this.inputP1)
-    }
-
-    suit(inputUser) {
-        this.inputP1 = inputUser;
-        if (inputUser === "gunting") {
-            if (this.pool[this.rand] === inputUser) {
-                this.result = "seri";
-            } else if (this.pool[this.rand] === "kertas") {
-                this.result = "menang"
-            } else if (this.pool[this.rand] === "batu") {
-                this.result = "kalah"
-            }
-        } else if (inputUser === "batu") {
-            if (this.pool[this.rand] === inputUser) {
-                this.result = "seri";
-            } else if (this.pool[this.rand] === "kertas") {
-                this.result = "kalah"
-            } else if (this.pool[this.rand] === "gunting") {
-                this.result = "menang"
-            }
-        } else if (inputUser === "kertas") {
-            if (this.pool[this.rand] === inputUser) {
-                this.result = "seri";
-            } else if (this.pool[this.rand] === "gunting") {
-                this.result = "kalah"
-            } else if (this.pool[this.rand] === "batu") {
-                this.result = "menang"
-            }
-        }
-        console.log (this.result);
-    }
+    dataToDB.forEach((x) => {
+        counts[x] = (counts[x] || 0) + 1;
+    })
+    return counts;
 }
 
+let store = getResult(dataToDB)
 
-const game = new Suitclass()
 
-/*
-setTimeout(() => {
-    game.match('gunting');
-    game.getInputP1();
-    game.getInputCOM();
-    
-}, 1000)
-*/
 
-game.suit('gunting');
-game.getInputP1();
-game.getInputCOM();
+
+let tes = 0
+Object.keys(store)  
+    .forEach((k )=>{ 
+        if (tes <= store[k])
+            tes = store[k]
+            console.log(k)
+        )});
